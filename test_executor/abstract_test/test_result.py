d85d3554057362df3a9df2344d5e4b6328d8d927
cohenid1 <idan1.cohen@intel.com>
2021-11-30 08:00:49 +0200

    Add result listeners capability

--- test_executor/abstract_test/test_result.py
+++ test_executor/abstract_test/test_result.py
@@ -4,16 +4,23 @@ from typing import List
 
 
 class TestVerdict(Enum):
+    """
+    Enum for test verdicts
+    """
     PASSED = "Passed"
     FAILED = "Failed"
     ABORTED = "Aborted"
 
 
 class TestResult(object):
+    """
+    This class is a class data that contains data about a test
+    """
     verdict: TestVerdict
     failure_reasons: List[str]
 
-    def __init__(self):
+    def __init__(self, test_number: int):
         self.verdict = TestVerdict.PASSED
         self.failure_reasons = []
         self.test_log = ""
+        self.test_number = test_number
