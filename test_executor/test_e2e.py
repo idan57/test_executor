bc97bae508a4d29ee385a36f1c90acbd198585c2
cohenid1 <idan1.cohen@intel.com>
2021-11-30 18:42:57 +0200

    fix e2e flow to enable testing

--- test_executor/test_e2e.py
+++ test_executor/test_e2e.py
@@ -1,15 +1,13 @@
 import os
-import sys
-
-main_folder = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(main_folder)
 
 
 if __name__ == '__main__':
+
     from test_generation.test_executor import TestExecutor
+
     from test_generation.test_loader import TestLoader
 
-    tests_path = os.path.join(os.path.dirname(__file__), "mocked_tests")
+    tests_path = os.path.join(os.path.dirname(__file__), "tests")
     tests = TestLoader.load_tests(tests_path)
     executor = TestExecutor()
 
